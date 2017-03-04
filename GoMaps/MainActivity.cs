fd5804848f647a82a58dc62ed525d8580b51f1e9
TimeEstimation <min2211mku@gmail.com>
2017-03-04 13:43:36 +0300

    Rename project GoMap to GoMaps

---
+++ GoMaps/MainActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace GoMaps {
+    [Activity(Label = "GoMaps", MainLauncher = true, Icon = "@drawable/ic_launcher")]
+    public class MainActivity : Activity {
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
