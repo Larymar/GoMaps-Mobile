0dbbefe41655f6accc121d6d2030218a76a4e293
TimeEstimation <min2211mku@gmail.com>
2017-03-04 16:02:15 +0300

    Add lost files to commit

--- GoMaps/Activities/MainActivity.cs
+++ GoMaps/Activities/MainActivity.cs
@@ -7,8 +7,6 @@ namespace GoMaps.Activities {
     public class MainActivity : Activity {
         protected override void OnCreate(Bundle bundle) {
             base.OnCreate(bundle);
-
-            // Set our view from the "main" layout resource
             SetContentView (Resource.Layout.Main);
         }
     }
