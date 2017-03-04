5d5fdabd8b68d2c6d4b61efb9a37b495084e9ee2
TimeEstimation <min2211mku@gmail.com>
2017-03-04 15:17:11 +0300

    Add NUnit test for Autofac Container

---
+++ GoMaps.Tests/TestContainer.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+using GoMaps.Container;
+using GoMaps.Gallery;
+namespace GoMaps.Tests {
+    [TestFixture]
+    public class TestContainer {
+        [Test]
+        public void TestMethod() {
+            AutofacConfig.ConfigurateContainer();
+            IGalleryManager manager;
+            using (var scope = AutofacConfig.Container.BeginLifetimeScope()) {
+                manager = AutofacConfig.Container.Resolve<IGalleryManager>();
+            }
+            Assert.IsInstanceOf<GalleryManager>(manager);
+        }
+    }
+}
